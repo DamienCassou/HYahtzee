63ece947d8ed9ec05dfaabb8d131ce5cc74789f3
Damien Cassou <damien.cassou@gmail.com>
2010-08-22 10:25:52 +0200

    - Cleans all code - Adds rollDices

--- src/engine/engine.hs
+++ src/engine/engine.hs
@@ -4,86 +4,94 @@ module Game.HYahtzee.Engine where
 
 import Data.List
 import Test.HUnit 
+import Random
 
 type DiceVal = Int
 type Dices = (DiceVal,DiceVal,DiceVal,DiceVal,DiceVal)
 type Score = Int
 
+rollDices :: Int -> [IO DiceVal]
+rollDices 0 = []
+rollDices x = rollDice : rollDices (x - 1) where
+  rollDice = getStdRandom (randomR (1,6))
 
+{- Combination tests -}
 
 data CombinationResult = MakeCombinationResult [DiceVal] Score
                        deriving (Show,Eq)
 
-type CombinationTest = Dices -> Maybe CombinationResult
+type CombinationTest = Dices -> CombinationResult
 
 dices2List :: Dices -> [DiceVal]
 dices2List (a,b,c,d,e) = [a,b,c,d,e]
 
 hasOnePair :: CombinationTest
 hasOnePair (a,b,c,d,e)
-  | (a==b) = Just (MakeCombinationResult [a,b] (2*a))
-  | (b==c) = Just (MakeCombinationResult [b,c] (2*b))
-  | (c==d) = Just (MakeCombinationResult [c,d] (2*c))
-  | (d==e) = Just (MakeCombinationResult [d,e] (2*d))
-  | otherwise = Nothing
+  | a==b      = MakeCombinationResult [a,b] (2*a)
+  | b==c      = MakeCombinationResult [b,c] (2*b)
+  | c==d      = MakeCombinationResult [c,d] (2*c)
+  | d==e      = MakeCombinationResult [d,e] (2*d)
+  | otherwise = MakeCombinationResult [] 0
 
 {- xxyyz, xyyzz, xxyzz -}
 hasTwoPairs :: CombinationTest
 hasTwoPairs (a,b,c,d,e)
-  | (a==b && c==d && b/=c) = Just (MakeCombinationResult [a,b,c,d] (2*a+2*c))
-  | (b==c && d==e && c/=d) = Just (MakeCombinationResult [b,c,d,e] (2*b+2*d))
-  | (a==b && d==e && b/=d) = Just (MakeCombinationResult [a,b,d,e] (2*a+2*d))
-  | otherwise              = Nothing
+  | a==b && c==d && b/=c =  MakeCombinationResult [a,b,c,d] (2*a+2*c)
+  | b==c && d==e && c/=d =  MakeCombinationResult [b,c,d,e] (2*b+2*d)
+  | a==b && d==e && b/=d =  MakeCombinationResult [a,b,d,e] (2*a+2*d)
+  | otherwise            =  MakeCombinationResult [] 0
 
 {-xxxyz xyyyz xyzzz -}
 hasThreeOfAKind :: CombinationTest
 hasThreeOfAKind (a,b,c,d,e)
-  | (a==c && c/=d) = Just (MakeCombinationResult [a,b,c] (3*a))
-  | (b==d && d/=e) = Just (MakeCombinationResult [b,c,d] (3*b))
-  | (c==e && b/=c) = Just (MakeCombinationResult [c,d,e] (3*c))
-  | otherwise              = Nothing
-
-
+  | a==c && c/=d = MakeCombinationResult [a,b,c] (3*a)
+  | b==d && d/=e = MakeCombinationResult [b,c,d] (3*b)
+  | c==e && b/=c = MakeCombinationResult [c,d,e] (3*c)
+  | otherwise    = MakeCombinationResult [] 0
 
 {- xxxxy xyyyy -}
 hasFourOfAKind :: CombinationTest
 hasFourOfAKind (a,b,c,d,e)
-  | (a==d)    = Just (MakeCombinationResult [a,b,c,d] (4*a))
-  | (b==e)    = Just (MakeCombinationResult [b,c,d,e] (4*b))
-  | otherwise = Nothing
+  | a==d      = MakeCombinationResult [a,b,c,d] (4*a)
+  | b==e      = MakeCombinationResult [b,c,d,e] (4*b)
+  | otherwise = MakeCombinationResult [] 0
 
 {- xxxyy xxyyy -}
 hasFullHouse :: CombinationTest
 hasFullHouse (a,b,c,d,e)
-  | (a==c && d==e && c/=d) = Just (MakeCombinationResult [a,b,c,d,e] 25)
-  | (a==b && c==e && b/=c) = Just (MakeCombinationResult [a,b,c,d,e] 25)
-  | otherwise      = Nothing
+  | a==c && d==e && c/=d =  MakeCombinationResult [a,b,c,d,e] 25
+  | a==b && c==e && b/=c =  MakeCombinationResult [a,b,c,d,e] 25
+  | otherwise            =  MakeCombinationResult [] 0
                      
 hasSmallStraight :: CombinationTest
 hasSmallStraight dices = let ldices = nub (dices2List dices)
-                         in case (length ldices) of
-                            4 -> Just (MakeCombinationResult ldices 30)
-                            5 -> Just (MakeCombinationResult (tail ldices) 30)
-                            _ -> Nothing
+                         in case ldices of
+                            [1,2,3,4] ->  (MakeCombinationResult ldices 30)
+                            [2,3,4,5] ->  (MakeCombinationResult ldices 30)
+                            [3,4,5,6] ->  (MakeCombinationResult ldices 30)
+                            [1,2,3,4,5] ->  (MakeCombinationResult (tail ldices) 30)
+                            [2,3,4,5,6] ->   (MakeCombinationResult (tail ldices) 30)
+                            _ ->  (MakeCombinationResult [] 0)
                             
 hasLargeStraight :: CombinationTest
 hasLargeStraight dices = let ldices = nub (dices2List dices)
-                         in case (length ldices) of
-                            5 -> Just (MakeCombinationResult ldices 40)
-                            _ -> Nothing
+                         in case ldices of
+                            [1,2,3,4,5] ->  (MakeCombinationResult ldices 40)
+                            [2,3,4,5,6] ->  (MakeCombinationResult ldices 40)
+                            _ ->  (MakeCombinationResult [] 0)
 
 hasYahtzee :: CombinationTest
 hasYahtzee (a,b,c,d,e)
-  | (a==e)    = Just (MakeCombinationResult [a,b,c,d,e] 50)
-  | otherwise = Nothing
+  | a==e      = MakeCombinationResult [a,b,c,d,e] 50
+  | otherwise = MakeCombinationResult [] 0
 
 hasChance :: CombinationTest
-hasChance (a,b,c,d,e) = Just (MakeCombinationResult [a,b,c,d,e] (a+b+c+d+e))
+hasChance (a,b,c,d,e) = MakeCombinationResult [a,b,c,d,e] (a+b+c+d+e)
 
 
 countXs :: DiceVal -> CombinationTest
 countXs x dices = let xs = filter (== x) (dices2List dices)
-                  in Just (MakeCombinationResult xs (sum xs))
+                  in  (MakeCombinationResult xs (sum xs))
 
 has1s :: CombinationTest
 has1s = countXs 1
@@ -103,73 +111,71 @@ has5s = countXs 5
 has6s :: CombinationTest
 has6s = countXs 6
 
+{- Unit Tests -}
+
+assertTestResult :: CombinationTest -> Dices -> [DiceVal] -> Score -> Assertion
+assertTestResult providedTest providedDices expectedDices expectedScore =
+  assertEqual ""
+   (MakeCombinationResult expectedDices expectedScore)
+   (providedTest providedDices)
+
 testHasOnePair :: Test
 testHasOnePair = TestCase (do
-                              assertEqual ""
-                                (Just (MakeCombinationResult [1,1] 2))
-                                (hasOnePair (1,1,2,3,4))
-                              assertEqual ""
-                                (Just (MakeCombinationResult [2,2] 4))
-                                (hasOnePair (1,2,2,3,4))
-                              assertEqual ""
-                                (Just (MakeCombinationResult [2,2] 4))
-                                (hasOnePair (1,2,2,2,2)))
-
+                              assertTestResult
+                                hasOnePair (1,1,2,3,4) [1,1] 2
+                              assertTestResult
+                                hasOnePair (1,2,2,3,4) [2,2] 4
+                              assertTestResult
+                                hasOnePair (1,2,2,2,2) [2,2] 4)
+                 
 testHasTwoPairs :: Test
 testHasTwoPairs= TestCase (do
-                              assertEqual ""
-                                (Just (MakeCombinationResult [1,1,2,2] 6))
-                                (hasTwoPairs (1,1,2,2,4))
-                              assertEqual ""
-                                (Nothing)
-                                (hasTwoPairs (1,1,1,1,2))
-                              assertEqual ""
-                                (Nothing)
-                                (hasTwoPairs (1,1,1,1,1)))
+                              assertTestResult
+                                hasTwoPairs (1,1,2,2,4) [1,1,2,2] 6
+                              assertTestResult
+                                hasTwoPairs (1,1,1,1,2) [] 0
+                              assertTestResult 
+                                hasTwoPairs (1,1,1,1,1) [] 0)
                  
 testHasThreeOfAKind :: Test
-testHasThreeOfAKind  = TestCase (assertEqual ""
-                                 (Just (MakeCombinationResult [1,1,1] 3))
-                                 (hasThreeOfAKind (1,1,1,2,4)))
+testHasThreeOfAKind  = TestCase (assertTestResult
+                                 hasThreeOfAKind (1,1,1,2,4) [1,1,1] 3)
                        
 testHasFourOfAKind :: Test
-testHasFourOfAKind = TestCase (assertEqual ""
-                               (Just (MakeCombinationResult [1,1,1,1] 4))
-                               (hasFourOfAKind (1,1,1,1,4)))
-                      
+testHasFourOfAKind = TestCase (assertTestResult
+                               hasFourOfAKind (1,1,1,1,4) [1,1,1,1] 4)
+                               
 testHasFullHouse :: Test
 testHasFullHouse = TestCase (do
-                                assertEqual ""
-                                  (Just (MakeCombinationResult [1,1,1,2,2] 25))
-                                  (hasFullHouse (1,1,1,2,2))
-                                assertEqual ""
-                                  Nothing
-                                  (hasFullHouse (1,1,1,1,1)))
+                                assertTestResult
+                                  hasFullHouse (1,1,1,2,2) [1,1,1,2,2] 25
+                                assertTestResult
+                                  hasFullHouse (1,1,1,1,1) [] 0)
                     
 testHasSmallStraight :: Test
 testHasSmallStraight = TestCase (do
-                                    assertEqual ""
-                                      (Just (MakeCombinationResult [1,2,3,4] 30))
-                                      (hasSmallStraight (1,2,3,4,4))
-                                    assertEqual ""
-                                      (Just (MakeCombinationResult [2,3,4,5] 30))
-                                      (hasSmallStraight (1,2,3,4,5)))
-                        
+                                    assertTestResult
+                                      hasSmallStraight (1,2,3,4,4) [1,2,3,4] 30
+                                    assertTestResult
+                                      hasSmallStraight (1,2,3,4,5) [2,3,4,5] 30
+                                    assertTestResult
+                                      hasSmallStraight (1,2,4,5,6) [] 0)
+                       
 testHasLargeStraight :: Test
-testHasLargeStraight = TestCase (assertEqual ""
-                                 (Just (MakeCombinationResult [1,2,3,4,5] 40))
-                                 (hasLargeStraight (1,2,3,4,5)))
-
+testHasLargeStraight = TestCase (do
+                                    assertTestResult
+                                      hasLargeStraight (1,2,3,4,5) [1,2,3,4,5] 40
+                                    assertTestResult
+                                      hasLargeStraight (1,2,3,5,6) [] 0)
+                       
 testHasYahtzee :: Test
-testHasYahtzee = TestCase (assertEqual ""
-                           (Just (MakeCombinationResult [2,2,2,2,2] 50))
-                           (hasYahtzee (2,2,2,2,2)))
-
+testHasYahtzee = TestCase (assertTestResult
+                           hasYahtzee (2,2,2,2,2) [2,2,2,2,2] 50)
+                           
 testHas1s :: Test
-testHas1s = TestCase (assertEqual ""
-                      (Just (MakeCombinationResult [1,1,1] 3))
-                      (has1s (1,1,1,2,3)))
-
+testHas1s = TestCase (assertTestResult
+                      has1s (1,1,1,2,3) [1,1,1] 3)
+                      
 tests :: Test
 tests = TestList [
   TestLabel "testHasOnePair" testHasOnePair
