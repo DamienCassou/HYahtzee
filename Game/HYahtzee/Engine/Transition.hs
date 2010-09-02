ccd85810af3351e6efc3d5e5cab0d5cc38959720
Damien Cassou <damien.cassou@gmail.com>
2010-09-02 22:51:01 +0200

    Implements a transition engine to simplify implementation of the yatzee logic

---
+++ Game/HYahtzee/Engine/Transition.hs
@@ -0,0 +1,34 @@
+{-# OPTIONS -Wall #-}
+
+module Game.HYahtzee.Engine.Transition where
+
+data Transition a = TransNorm (a -> a) (a -> IO a) (Choice a)
+                  | TransFinal (a -> a) (a -> IO a)
+                  
+data Choice a = Choice (a -> Bool) (Transition a) (Transition a)
+
+executeTransition :: a -> Transition a -> IO a
+executeTransition state (TransNorm f io choice) =
+  do newState <- (io . f) state
+     executeChoice newState choice
+executeTransition state (TransFinal f io) =
+  (io . f) state
+  
+executeChoice :: a -> Choice a -> IO a
+executeChoice state (Choice test t1 t2) =
+  if (test state)
+  then executeTransition state t1
+  else executeTransition state t2
+       
+
+{- Example -}
+mytran :: Transition Integer
+mytran = (TransNorm
+          (\x -> x - 1)
+          (\x -> do {(putStrLn . show) x; return x})
+          (Choice (> 1)
+           mytran
+           (TransFinal id (\x -> return x))))
+         
+main :: IO ()
+main = executeTransition 6 mytran >> return ()
